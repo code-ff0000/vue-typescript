a3ba7682e0e6c55103d49474587604b270e0959d
code-ff0000 <github@ot24e5t.8shield.net>
2023-06-18 18:18:50 -0500

    Refactor types into Type files, and define event parameters

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
 import TheHeader from "@/components/TheHeader.vue";
 import EntryEditor from "./components/EntryEditor.vue";
 import EntryCard from "@/components/EntryCard.vue";
+
+import { reactive } from "vue";
+
+import type User from "@/types/User";
+
+const user: User = reactive({
+  id: 1,
+  username: "code-ff0000",
+  settings: [],
+});
+
+console.log(user.username);
 </script>
 
 <template>
--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -1,13 +1,38 @@
 <script lang="ts" setup>
 import EmojiField from "@/components/EmojiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
+
+import { computed, ref } from "vue";
+
+import type Emoji from "@/types/Emoji";
+
+const text = ref("");
+const emoji = ref<Emoji | null>(null);
+const charCount = computed(() => text.value.length);
+const maxChars = 280;
+
+const handleTextInput = (e: Event) => {
+  const textarea = e.target as HTMLTextAreaElement;
+
+  if (textarea.value.length <= maxChars) {
+    text.value = textarea.value;
+  } else {
+    text.value = textarea.value = textarea.value.substring(0, maxChars);
+  }
+}
 </script>
+
 <template>
   <form class="entry-form" @submit.prevent>
-    <textarea placeholder="New Journal Entry for danielkelly_io"></textarea>
-    <EmojiField />
+    <textarea
+      :value="text"
+      placeholder="New Journal Entry for danielkelly_io"
+      @keyup="handleTextInput"
+    >
+    </textarea>
+    <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>0 / 280</span>
+      <span>{{ charCount }} / {{ maxChars }}</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
